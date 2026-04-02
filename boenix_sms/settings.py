c1072c9f0f1748a9014a60c353585245f0caccdb
kevinkemboi4250@gmail.com <45280598+chichikemboi@users.noreply.github.com>
2026-04-02 22:02:51 +0300

    fix allowed host

--- boenix_sms/settings.py
+++ boenix_sms/settings.py
@@ -8,7 +8,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 SECRET_KEY = env('SECRET_KEY', default='django-insecure-change-me-in-production')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1']) + ['sms-2-8i0m.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
